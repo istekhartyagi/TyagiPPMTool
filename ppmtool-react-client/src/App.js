d7b286ed9db4080f7169c21899e10d7433438621
Istekhar Tyagi <istekhartyagi@gmail.com>
2019-11-24 17:08:30 +0530

    basic layout in dashboard, project item, header, & add the awesome.font etc..

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Layout/Header";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
